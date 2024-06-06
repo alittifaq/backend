1e23252a276a19dbed2c16b000afdb1929180d10
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-06-06 14:59:48 +0700

    backend|temp

--- route/route.go
+++ route/route.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gocroot/config"
 	"github.com/gocroot/controller"
+	"github.com/gocroot/helper"
 )
 
 func URL(w http.ResponseWriter, r *http.Request) {
@@ -17,7 +18,10 @@ func URL(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case method == "GET" && path == "/":
 		controller.GetHome(w, r)
-
+	case method == "GET" && path == "/refresh/token":
+		controller.GetNewToken(w, r)
+	case method == "POST" && helper.URLParam(path, "/webhook/nomor/:nomorwa"):
+		controller.PostInboxNomor(w, r)
 	default:
 		controller.NotFound(w, r)
 	}
